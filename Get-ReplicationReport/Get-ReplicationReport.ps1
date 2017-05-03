eb4e7fd0b0ea7d59f1d59be2e93f5284b7b8657d
Bill Long <bilong@microsoft.com>
2017-05-03 02:47:19 -0500

    Update Get-ReplicationReport and remove duplicate

--- Get-ReplicationReport/Get-ReplicationReport.ps1
+++ Get-ReplicationReport/Get-ReplicationReport.ps1
@@ -1,37 +1,52 @@
-# Get-ReplicationReport
-# 
-# PF replication report script with primary goals of performance and terse output.
-# 
-# This script will run Get-PublicFolderStatistics against all specified servers
-# simultaneously, and will begin processing the results as soon as any of those
-# tasks complete. This significantly improves performance in large public folder
-# deployments.
-# 
-# The output ONLY contains folders where there is an item count difference between
-# replicas. Folders where all replicas have the same item count are not included.
-# 
-# Syntax examples:
-# 
-# Display the report in the shell:
-# 
-# .\Get-ReplicationReport -Servers SERVER1,SERVER2,SERVER3
-# 
-# Write the report to a CSV file:
-# 
-# .\Get-ReplicationReport -Servers SERVER1,SERVER2,SERVER3 -Outfile C:\replreport.csv
-# 
-# To ignore any folders in a DeletePending state (useful when you have a lot of folders
-# pending deletion and generating false positives):
-# 
-# .\Get-ReplicationReport -Servers SERVER1,SERVER2,SERVER3 -Outfile C:\replreport.csv -SkipDeletePending $true
-# 
+#requires -version 2
+<#
+.SYNOPSIS
 
-param([string[]]$Servers, [string]$Outfile, [bool]$SkipDeletePending)
+    PF replication report script with primary goals of performance and terse output.
 
-if ($Servers.Length -lt 2)
+.DESCRIPTION
+
+    This script will run Get-PublicFolderStatistics against all specified servers
+    simultaneously, and will begin processing the results as soon as any of those
+    tasks complete. This significantly improves performance in large public folder
+    deployments.
+
+    The output ONLY contains folders where there is an item count difference between
+    replicas. Folders where all replicas have the same item count are not included.
+
+.INPUTS
+
+.EXAMPLE
+
+    Compare three servers and write the output to the folder where the script resides:
+    
+    .\Get-ReplicationReport -Servers SERVER1,SERVER2,SERVER3
+    
+    Write the report to a different directory:
+    
+    .\Get-ReplicationReport -Servers SERVER1,SERVER2,SERVER3 -OutFolder C:\Reports
+
+    To ignore any public folders in a DeletePending state (useful when you have a lot of 
+    folders pending deletion and generating false positives):
+    
+    .\Get-ReplicationReport -Servers SERVER1,SERVER2,SERVER3 -SkipDeletePending $true
+
+    To compare against the previous CSV and eliminate more false positives:
+
+    .\Get-ReplicationReport -Servers SERVER1,SERVER2,SERVER3 -SkipDeletePending $true -ComparePrevious $true
+#>
+
+param([string[]]$Servers, [string]$InFolder, [string]$OutFolder, [bool]$SkipDeletePending, [bool]$ComparePrevious)
+
+if ($Servers.Length -lt 2 -and [string]::IsNullOrEmpty($InFolder))
 {
     Write-Host "You must provide at least 2 server names."
     return
+}
+
+if ([string]::IsNullOrEmpty($OutFolder))
+{
+    $OutFolder = split-path -parent $MyInvocation.MyCommand.Definition
 }
 
 Get-Job | Remove-Job
@@ -39,29 +54,44 @@ Get-Job | Remove-Job
 $startTime = Get-Date
 $jobs = @()
 
-foreach ($server in $Servers)
+if ($Servers.Length -gt 0)
 {
-    $scriptBlock = {
-        param($serverArg)
-        $WarningPreference = "SilentlyContinue"
-        Import-PSSession (New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$serverArg/powershell" -Authentication Kerberos) | Out-Null
-        Get-PublicFolderStatistics -Server $server -ResultSize Unlimited | Select FolderPath,ItemCount,IsDeletePending,DatabaseName
+    foreach ($server in $Servers)
+    {
+        $scriptBlock = {
+            param($serverArg)
+            $WarningPreference = "SilentlyContinue"
+            Import-PSSession (New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$serverArg/powershell" -Authentication Kerberos) | Out-Null
+            Get-PublicFolderStatistics -Server $server -ResultSize Unlimited | Select-Object FolderPath,ItemCount,IsDeletePending,DatabaseName
+        }
+        
+        $jobs += Start-Job $scriptBlock -ArgumentList $server -Name $server
     }
-    
-    $jobs += Start-Job $scriptBlock -ArgumentList $server -Name $server
+}
+else 
+{
+    foreach ($file in (Get-ChildItem -Path $InFolder -Filter *.csv))
+    {
+        $scriptBlock = {
+            param($fileArg)
+            return Import-Csv $fileArg.FullName
+        }
+
+        $jobs += Start-Job $scriptBlock -ArgumentList $file -Name $file
+    }
 }
 
 Write-Host "Waiting for jobs to complete..."
 $folders = New-Object 'System.Collections.Generic.Dictionary[string, System.Collections.Generic.Dictionary[string, int]]'
 $dbNames = @()
+$startingJobCount = $jobs.Length
+$alreadyDoneCount = 0
 while (Get-Job)
 {
-
-    $completedJobs = Get-Job | WHERE { $_.State -eq "Completed" }
+    $completedJobs = Get-Job | Where-Object { $_.State -ne "Running" }
     if ($completedJobs -eq $null)
     {
-        $alreadyDoneCount = $Servers.Length - (Get-Job).Length
-        Write-Progress -Activity "Waiting for Get-PublicFolderStatistics to finish." -PercentComplete ($alreadyDoneCount / $Servers.Length * 100) -Status ("$alreadyDoneCount / " + $Servers.Length + " servers complete.")
+        Write-Progress -Activity "Waiting for job to finish." -PercentComplete ($alreadyDoneCount / $startingJobCount * 100) -Status ("$alreadyDoneCount / " + $startingJobCount + " jobs processed.")
         Start-Sleep 1
         continue
     }
@@ -69,7 +99,7 @@ while (Get-Job)
     $completedJobs
     foreach ($job in $completedJobs)
     {
-        Write-Host "Receiving job" $job.Name
+        Write-Progress -Activity ("Receiving job " + $job.Name) -PercentComplete ($alreadyDoneCount / $startingJobCount * 100) -Status ("$alreadyDoneCount / " + $startingJobCount + " jobs processed.")
         $statistics = Receive-Job $job
         Remove-Job $job
         $total = $statistics.Count
@@ -80,11 +110,18 @@ while (Get-Job)
 
         $dbNames += $statistics[0].DatabaseName
         $current = 0
+        $timer = [System.Diagnostics.Stopwatch]::StartNew()
+        $lastProgressMsec = 0
         foreach ($stat in $statistics)
         {
             ++$current
-            Write-Progress -Activity ("Processing statistics from " + $stat.DatabaseName) -PercentComplete ($current / $total * 100) -Status "$current / $total"
-            if ($SkipDeletePending -and $stat.IsDeletePending)
+            if ($timer.ElapsedMilliseconds - $lastProgressMsec -gt 999)
+            {
+                Write-Progress -Activity ("Processing statistics from " + $stat.DatabaseName) -PercentComplete ($current / $total * 100) -Status "$current / $total"
+                $lastProgressMsec = $timer.ElapsedMilliseconds
+            }
+
+            if ($SkipDeletePending -eq $true -and $stat.IsDeletePending -eq $true)
             {
                 continue
             }
@@ -109,22 +146,60 @@ while (Get-Job)
                 $folders.Add($stat.FolderPath, $itemCountForReplicas)
             }
         }
+
+        $timer.Stop()
+        $alreadyDoneCount = $startingJobCount - (Get-Job).Length
     }
 }
 
 Write-Host
 Write-Host "Comparing item counts..."
+$previousData = $null
+if ($ComparePrevious)
+{
+    $previousCsvFile = Get-ChildItem *.csv | Sort-Object CreationTime -Descending | Select-Object -First 1
+    if ($previousCsvFile -eq $null)
+    {
+        Write-Warning "ComparePrevious is true, but no previous CSV file was found in $OutFolder"
+    }
+    else 
+    {
+        $previousData = Import-Csv $previousCsvFile
+    }
+}
+
+$dbNames = $dbNames | Sort-Object
 $foldersNotInSync = @()
 $total = $folders.Keys.Count
 $current = 0
+$timer = [System.Diagnostics.Stopwatch]::StartNew()
+$lastProgressMsec = 0
 foreach ($folderPath in $folders.Keys)
 {
     ++$current
-    Write-Progress -Activity "Comparing item counts" -PercentComplete ($current / $total * 100) -Status "$current / $total"
+    if ($timer.ElapsedMilliseconds - $lastProgressMsec -gt 999)
+    {
+        Write-Progress -Activity "Comparing item counts" -PercentComplete ($current / $total * 100) -Status "$current / $total"
+        $lastProgressMsec = $timer.ElapsedMilliseconds
+    }
+
     $folder = $folders[$folderPath]
-    $measurement = $folder.Values | measure -Maximum -Minimum
+    $measurement = $folder.Values | Measure-Object -Maximum -Minimum
     if ($measurement.Minimum -ne $measurement.Maximum)
     {
+        if ($previousData -ne $null)
+        {
+            $previousEntry = $previousData | Where-Object { $_.FolderPath -eq $folderPath }
+            if ($previousEntry -ne $null)
+            {
+                $previousMeasurement = $previousEntry.Value | Measure-Object -Minimum
+                if ($previousMeasurement.Minimum -ne $measurement.Minimum)
+                {
+                    continue
+                }
+            }
+        }
+
         $reportObj = New-Object PSObject
         Add-Member -InputObject $reportObj -MemberType NoteProperty -Name FolderPath -Value $folderPath
         foreach ($dbName in $dbNames)
@@ -144,16 +219,12 @@ foreach ($folderPath in $folders.Keys)
     }
 }
 
+$timer.Stop()
 $endTime = Get-Date
 
-if ($Outfile.Length -gt 0)
-{
-    $foldersNotInSync | Export-Csv $Outfile -NoTypeInformation
-}
-else
-{
-    $foldersNotInSync
-}
+$fileName = "ReplicationReport" + [DateTime]::Now.ToString("yyyyMMddHHmm") + ".csv"
+$fullName = Join-Path $OutFolder $fileName
+$foldersNotInSync | Sort-Object FolderPath | Export-Csv $fullName -NoTypeInformation -Encoding Unicode
 
 Write-Host "Folders found: $total"
 Write-Host "Folders not in sync: " $foldersNotInSync.Length
